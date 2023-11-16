9418de82ca1b5850fc7e2a690a248cde3a64bff0
wulonglong <wulonglong.917@gmail.com>
2023-11-16 16:10:02 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -1,19 +1,25 @@
 package com.sunnyweather.android.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.sunnyweather.android.R
 import com.sunnyweather.android.logic.model.Weather
 import com.sunnyweather.android.logic.model.getSky
+import kotlinx.android.synthetic.main.activity_weather.drawerLayout
+import kotlinx.android.synthetic.main.activity_weather.swipeRefresh
 import kotlinx.android.synthetic.main.activity_weather.weatherLayout
 import kotlinx.android.synthetic.main.forecast.forecastLayout
 import kotlinx.android.synthetic.main.life_index.carWashingText
@@ -25,6 +31,7 @@ import kotlinx.android.synthetic.main.life_index.ultravioletText
 import kotlinx.android.synthetic.main.now.currentAQI
 import kotlinx.android.synthetic.main.now.currentSky
 import kotlinx.android.synthetic.main.now.currentTemp
+import kotlinx.android.synthetic.main.now.navBtn
 import kotlinx.android.synthetic.main.now.nowLayout
 import kotlinx.android.synthetic.main.now.placeName
 import java.text.SimpleDateFormat
@@ -58,8 +65,41 @@ class WeatherActivity : AppCompatActivity() {
                 Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
+            swipeRefresh.isRefreshing = false
         })
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary)
+        refreshWeather()
+        swipeRefresh.setOnRefreshListener {
+            refreshWeather()
+        }
+        navBtn.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+            }
+
+            override fun onDrawerClosed(drawerView: View) {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE)
+                        as InputMethodManager
+                manager.hideSoftInputFromWindow(
+                    drawerView.windowToken,
+                    InputMethodManager.HIDE_NOT_ALWAYS
+                )
+            }
+
+            override fun onDrawerStateChanged(newState: Int) {
+            }
+
+        })
+    }
+
+    public fun refreshWeather() {
         viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+        swipeRefresh.isRefreshing = true
     }
 
     private fun showWeatherInfo(weather: Weather) {
